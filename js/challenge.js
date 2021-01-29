7b75a4f111144c8d97d8e3d908e9cd1575c5ad4b
jbrasko <jbrasko0508@gmail.com>
2021-01-29 15:50:53 -0600

    commit tree d9f829962689ea3656eecd0ac45c28651d68e6d4

--- js/challenge.js
+++ js/challenge.js
@@ -3,6 +3,7 @@ const minusButton = document.getElementById("minus")
 const plusButton = document.getElementById("plus")
 const pauseButton = document.getElementById("pause")
 const heartButton = document.getElementById("heart")
+const likeList = document.querySelector(".likes")
 let intId = setInterval(countUp, 1000)
 
 function countUp() {
@@ -19,17 +20,34 @@ function addToCounter() {
     ++counter.innerHTML
 }
 
+heartButton.addEventListener("click", likeCount)
+function likeCount() {
+    let number = counter.innerHTML
+    let foundLi = likeList.querySelector(`li[data-id="${number}"]`)
+    if (!foundLi) {
+        let li = document.createElement(`li`)
+        li.innerHTML = `${number} has been liked <span> 1 </span> times`
+        li.dataset.id = number
+        likeList.append(li)
+    } else {
+        let spanNum = foundLi.children[0]
+        let newValue = parseInt(spanNum.innerText)
+        spanNum.innerText = ++newValue
+
+    }
+
+}
+
 pauseButton.addEventListener("click", changeCounter)
 function changeCounter(event) {
-    debugger
     if (event.target.innerHTML === " pause ") {
-        window.clearInterval(intId)
+        clearInterval(intId)
         pauseButton.innerHTML = "resume"
         minusButton.disabled = true
         plusButton.disabled = true
         heartButton.disabled = true
     } else {
-        let intId = setInterval(countUp, 1000)
+        intId = setInterval(countUp, 1000)
         pauseButton.innerHTML = " pause "
         minusButton.disabled = false
         plusButton.disabled = false
